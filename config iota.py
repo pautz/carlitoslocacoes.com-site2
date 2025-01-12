6af97a3e9212d71609a0a1ac68466f3948ef3923
Carlito Pautz <37116894+pautz@users.noreply.github.com>
2025-01-12 15:31:37 -0300

    Update config iota.py
    Signed-off-by: Carlito Pautz <37116894+pautz@users.noreply.github.com>

--- config iota.py
+++ config iota.py
@@ -1,5 +1,15 @@
+pip install binance-connector
 pip install python-binance
 pip install requests
 pip install mysql-connector-python
 pip install qrcode[pil]
 pip install keyboard
+
+set no windows variaveis de ambiente e sistema
+Painel de Controle\Sistema e Segurança
+Exibir configurações avançadas do sistema
+Váriaveis de Ambiente...
+adicionar em variaveis de usario e sistema
+BINANCE_API_KEY = valores binance
+BINANCE_API_SECRET = valores binance
+
