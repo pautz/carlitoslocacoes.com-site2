4b8e7eb88cce2cb38fdb732bdb96fdec9bc2ba30
Carlito Pautz <37116894+pautz@users.noreply.github.com>
2025-01-16 12:22:21 -0300

    Update iota.php
    Signed-off-by: Carlito Pautz <37116894+pautz@users.noreply.github.com>

--- iota.php
+++ iota.php
@@ -1,33 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cálculo de Preços</title>
+</head>
+<body>
+    <a href="welcome.php" class="btn btn-primary btn-xl">Início</a>
+    <canvas id="qrcode"></canvas>
+
+    <form id="calculoForm" method="post">
+        <div class="page-header">
+            <h1>Olá, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>Seja Bem Vindo.</h1>
+            <input type="hidden" name="eq_user" required placeholder="Nome do cadastrante" value="<?php echo htmlspecialchars($_SESSION["username"]); ?>" id="input-hidden" readonly>
+        </div>
+        1 TYU = 1.54 reais<br>
+        <label for="qtu">TYU:</label>
+        <input type="text" id="qtu" name="qtu">
+        <br>
+        <button type="submit">Calcular</button>
+    </form>
+</body>
+</html>
 <?php
 // Initialize the session
 session_start();
- 
+
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
 
 // Functions
-function calcularValores($o_t, $p_y, $q_u) {
-    $o_tyu = $o_t;
-    $p_tyu = $p_y * 60;
-    $q_tyu = $q_u * 3600;
-    return [$o_tyu, $p_tyu, $q_tyu];
-}
-
-function calcularPrecos($o_tyu, $p_tyu, $q_tyu, $precoPorSegundo = 0.0083) {
-    $preco_o_tyu = $o_tyu * $precoPorSegundo;
-    $preco_p_tyu = $p_tyu * $precoPorSegundo;
-    $preco_q_tyu = $q_tyu * $precoPorSegundo;
-    return [$preco_o_tyu, $preco_p_tyu, $preco_q_tyu];
-}
-
-function salvarDadosNoBanco($precoTotal) {
+function salvarDadosNoBanco($precoTotal)
+{
     $host = 'localhost';
-    $user = 'SEUUSER';
-    $password = 'SEUPWD';
-    $database = 'SUADB';
+    $user = '';
+    $password = '';
+    $database = '';
     $mysqli = new mysqli($host, $user, $password, $database);
 
     if ($mysqli->connect_error) {
@@ -49,92 +64,42 @@ function salvarDadosNoBanco($precoTotal) {
     $mysqli->close();
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $o_t = floatval($_POST['o_t']);
-    $p_y = floatval($_POST['p_y']);
-    $q_u = floatval($_POST['q_u']);
+$enderecoIota = 'iota1qpg8a6dr4zlegluepxkasaxddsu7pxr4rlpcjskg8ph6kj9nkf6dys2jt69';
 
-    [$o_tyu, $p_tyu, $q_tyu] = calcularValores($o_t, $p_y, $q_u);
-    [$preco_o_tyu, $preco_p_tyu, $preco_q_tyu] = calcularPrecos($o_tyu, $p_tyu, $q_tyu);
-    $precoTotal = $preco_o_tyu + $preco_p_tyu + $preco_q_tyu;
-    $tyus = ($o_tyu/60+$p_tyu/60+$q_tyu/60)/3;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $qtu = floatval($_POST['qtu']);
+    $precoTotal = $qtu * 1.54;
+
     echo "
     <div id='resultado'>
-        <p>o_tyu: {$o_tyu} s</p>
-        <p>p_tyu: {$p_tyu} s</p>
-        <p>q_tyu: {$q_tyu} s</p>
-        <p>Preço de o_tyu: {$preco_o_tyu} reais</p>
-        <p>Preço de p_tyu: {$preco_p_tyu} reais</p>
-        <p>Preço de q_tyu: {$preco_q_tyu} reais</p>
-        <p>Tyus: {$tyus} tyus</p>
+        <p>{$qtu} TYU</p>
         <p>Preço total: {$precoTotal} reais</p>
-    </div>
-    <div id='qrcode'></div>
-    <script src='https://cdn.jsdelivr.net/npm/qrcode/build/qrcode.min.js'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.js'></script>
-    <script>
-        const enderecoIota = 'iota1qpg8a6dr4zlegluepxkasaxddsu7pxr4rlpcjskg8ph6kj9nkf6dys2jt69';
-        const precoTotal = {$precoTotal}.toFixed(8);
+    </div>";
 
-        const qrData = 'iota:' + enderecoIota + '?amount=' + precoTotal;
-        QRCode.toDataURL(qrData, function (err, url) {
-            if (err) throw err;
-            const qrDiv = document.getElementById('qrcode');
-            qrDiv.innerHTML = '<img src=\"' + url + '\">';
-        });
-
-        async function realizarPagamento() {
-            const apiKey = 'SUAAPIAQUI'; //API KEY BINANCE APAGAR //
-            const apiSecret = 'SUASECRETAQUI'; //API SECRET BINANCE APAGAR //
-            const timestamp = Date.now();
-            const query = `coin=IOTA&address=${enderecoIota}&amount=${precoTotal}&timestamp=${timestamp}`;
-            const signature = CryptoJS.HmacSHA256(query, apiSecret).toString(CryptoJS.enc.Hex);
-            const url = `https://api.binance.com/sapi/v1/capital/withdraw/apply?${query}&signature=${signature}`;
-
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'X-MBX-APIKEY': apiKey
-                }
-            });
-
-            const result = await response.json();
-            console.log(result);
-        }
-
-        realizarPagamento();
-    </script>
-    ";
-
+    // Save data to the database
     salvarDadosNoBanco($precoTotal);
+
+    // Generate QR code for deposit
+    echo "
+    <script src='https://cdn.jsdelivr.net/npm/qrcode@1.4.4/build/qrcode.min.js'></script>
+    <div id='qrcode'><script>
+        const enderecoIota = '{$enderecoIota}';
+        const precoTotal = parseFloat('{$precoTotal}').toFixed(8);
+        
+        const qrData = `iota:${enderecoIota}?amount=${precoTotal}`;
+        const qrDiv = document.getElementById('qrcode');
+        if (qrDiv) {
+            QRCode.toCanvas(document.getElementById('qrcode'), qrData, function (error) {
+                if (error) console.error(error)
+                console.log('success!');
+            });
+        } else {
+            console.error('Elemento com ID \"qrcode\" não encontrado.');
+        }
+    </script></div>
+    
+    ";
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <br>
-    <a href="welcome.php" class="btn btn-primary btn-xl">Início</a>
-    <title>Cálculo de Preços</title>
-</head>
-<body>
-        <form id="calculoForm" method="post">
-         <div class="page-header">
-            <h1>Olá, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Seja Bem Vindo.</h1>
-            <input type="text" name="eq_user" required placeholder="Nome do cadastrante" value="<?php echo htmlspecialchars($_SESSION["username"]); ?>" id="input-hidden" readonly>
-        </div>
-        10 tyus = 30 Minutos = 1800s = 14.94 R$<br>
-        <label for="o_t">Segundos (o_t):</label>
-        <input type="text" id="o_t" name="o_t">
-        <br>
-        <label for="p_y">Minutos (p_y):</label>
-        <input type="text" id="p_y" name="p_y">
-        <br>
-        <label for="q_u">Horas (q_u):</label>
-        <input type="text" id="q_u" name="q_u">
-        <br>
-        <button type="submit">Calcular</button>
-    </form>
-</body>
-</html>
+
